3ccf65f3441d762f5e56bc871e4d2f0469da6809
Andres98100 <andressolarte580@gmail.com>
2024-07-13 16:30:57 -0500

    Initial commit

---
+++ src/test/java/andres/forohub/ForohubApplicationTests.java
@@ -0,0 +1,13 @@
+package andres.forohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
